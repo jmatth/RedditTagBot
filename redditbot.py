841fda780bb136340d9a4948b80a01c429b57cd8
Josh Matthews <jmatth@nbcs.rutgers.edu>
2012-12-02 20:47:03 +0400

    Fixing regex matching. Now using nested for loops and dictionaries to define what to look for. TODO: make a config file.

--- redditbot.py
+++ redditbot.py
@@ -5,23 +5,41 @@ import re
 # Import os for touching lock files
 import os
 # Import datetime for timestamping lock files
-import datetime from datetime
+from datetime import datetime
+import sys
 
-if os.path.exists('/var/redditbot/mechanicalkeyboards.lock'):
+lockpath = '/var/redditbot/mechanicalkeyboards.lock'
+
+subreddit = 'mechanicalkeyboards'
+
+post_limit = 5
+
+if os.path.exists(lockpath):
 	print "Already running"
-	return
+	sys.exit()
 
 # Write a lockfile to prevent another instance from starting
 # while this one is still running.
-lockfile = open('/var/redditbot/mechanicalkeyboards.lock', 'w')
-lockfile.write('Bot instance started at: ' + datetime.now().strftime("%H:%M:%S %m/%d/%y"))
+lockfile = open(lockpath, 'w')
+lockfile.write('Bot instance started at: ' + datetime.now().strftime("%H:%M:%S %m/%d/%y") + "\n")
 
+reg_list = {}
+
+reg_list['geekwhack'] = {'title': re.compile('[Gg]eekhack'), 'url': re.compile('.*geekhack\.org.*'), 'css_class': 'geekwhack'}
+
+reg_list['imgur'] = {'title': re.compile('imgur'), 'url': re.compile('.*imgur.*'), 'css_class': 'imgur'}
+
+r = praw.Reddit(user_agent="TransistorRevolt testing the /r/mechanicalkeyboards bot.")
 
 r = praw.Reddit(user_agent="TransistorRevolt testing the mechanicalkeyboards bot.")
 
-r.login('username', 'password')
+hot = r.get_subreddit(subreddit).get_hot(limit=post_limit)
 
-top = r.get_subreddit('mechanicalkeyboards').get_hot(limit=5)
+for post in hot:
+	for check in reg_list:
+		if (reg_list[check]['url'].match(post.url) || reg_list[check]['title'].match(post.title)):
+			post.set_flair(flair_css_class=reg_list[check]['css_class'])
+			break
+	
 
-for x in top:
-	#TODO: write code
+os.remove(lockpath)
