63dc7b06776e847414fc6bcedc59215005ffc228
Josh Matthews <jmatth@nbcs.rutgers.edu>
2013-01-18 23:24:32 -0500

    Refactoring. Adding support for top and new posts.
    Config files should now contain hot_limit, top_limit, and new_limit
    instead of posts_limit.
    
    The code has been split up a bit into different functions, including a
    wrapper main function.

--- tagbot.py
+++ tagbot.py
@@ -1,5 +1,5 @@
+"""Python bot to tag posts on reddit.com"""
 # vim: set expandtab ts=4 sw=4 :
-# Reddit API
 import praw
 import re
 import os
@@ -11,117 +11,139 @@ import pymongo
 
 # Load the ini into a dictionary for either the
 # main config values or the tags to look for.
-def loadConfig(section='main'):
+def main():
+    """Wrapper method"""
+    main_config = load_config()
 
-    path = str(sys.path[0])
-    retDict = yaml.load(file(path+'/tagbot.yaml', 'r'))[section]
+    try:
+        lockpath = main_config['lockpath']
+        db_name = main_config['database']
 
-    return retDict
+    except KeyError:
+        print "Missing value from main_tagbot_config"
+        sys.exit()
 
-try:
-    mainConfig = loadConfig()
-except AttributeError:
-    print "Config file not found, not readable, or incomplete."
-    sys.exit()
+    # Write a lockfile to prevent another instance from starting
+    # while this one is still running, or exit if it already exits.
+    if not write_lockfile(lockpath):
+        sys.exit()
 
-try:
-    lockpath = mainConfig['lockpath']
-    database = mainConfig['database']
+    #Open connection to db_name
+    connection = pymongo.Connection()
+    database = connection[db_name]
 
-except KeyError:
-    print "Missing value from main_tagbot_config"
-    sys.exit()
+    #Now we load the subreddits we want to process.
+    #Underscore used in variable name to reduce the
+    #risk of confusing typos.
+    sub_reddits = load_config('subreddits')
 
-if os.path.exists(lockpath):
-    print "Already running"
-    sys.exit()
+    for subreddit_name, subreddit_options in sub_reddits.iteritems():
+        reddit = praw.Reddit(user_agent=subreddit_options['username']
+                             + " running the reddit tagbot for /r/"
+                             + subreddit_name + ".")
+        reddit.login(username=subreddit_options['username'],
+                     password=subreddit_options['password'])
+        subreddit = reddit.get_subreddit(subreddit_name)
 
-# Write a lockfile to prevent another instance from starting
-# while this one is still running.
-lockfile = open(lockpath, 'w')
-lockfile.write('Bot instance started at: ' +
-               datetime.now().strftime("%H:%M:%S %m/%d/%y") + "\n")
+        categories = get_categories(subreddit, subreddit_options)
 
-#Open connection to database
-connection = pymongo.Connection()
-db = connection[database]
+        for category in categories:
+            process_posts(category, subreddit_options['tags'],
+                          database[subreddit_name])
 
-#Now we load the subreddits we want to process.
-#Underscore used in variable name to reduce the
-#risk of confusing typos.
-sub_reddits = loadConfig('subreddits')
+    os.remove(lockpath)
 
-for subreddit in sub_reddits:
-    #Use a collection matching the subreddit name
-    collection = db[subreddit]
 
-    #Load subreddit specific configs
-    tag_list = sub_reddits[subreddit]['tags']
+def get_categories(subreddit, subreddit_options):
+    """Get a dictionary of iterators for the subreddit"""
+    return [subreddit.get_hot(limit=subreddit_options['hot_limit']),
+            subreddit.get_new(limit=subreddit_options['new_limit']),
+            subreddit.get_top(limit=subreddit_options['top_limit'])]
 
-    #Login to reddit
-    r = praw.Reddit(user_agent=sub_reddits[subreddit]['username']
-                    + " running the reddit tagbot for /r/"
-                    + subreddit + ".")
 
-    r.login(username=sub_reddits[subreddit]['username'],
-            password=sub_reddits[subreddit]['password'])
+def process_posts(posts, tags, collection):
+    """Check a list of posts"""
 
-    #Get hot posts
-    hot = (r.get_subreddit(subreddit)
-            .get_hot(limit=sub_reddits[subreddit]['post_limit']))
-
-    for post in hot:
+    for post in posts:
 
         #Skip post if it's already been checked.
         if (collection.find_one({'post_id': post.id})):
             continue
 
         matched = False
-        for check in tag_list:
+        for check in tags:
             #Break out if the previous iteration found a match
             if matched:
                 break
 
-            for condition in tag_list[check]['conditions']:
+            for condition in tags[check]['conditions']:
 
-                if ('url' in tag_list[check]['conditions'][condition]):
+                if ('url' in tags[check]['conditions'][condition]):
                     if not (re.match(
-                        tag_list[check]['conditions'][condition]['url'],
+                        tags[check]['conditions'][condition]['url'],
                             post.url, re.IGNORECASE)):
                         continue
 
-                if ('title' in tag_list[check]['conditions'][condition]):
+                if ('title' in tags[check]['conditions'][condition]):
                     if not (re.match(
-                        tag_list[check]['conditions'][condition]['title'],
+                        tags[check]['conditions'][condition]['title'],
                             post.title, re.IGNORECASE)):
                         continue
 
-                if ('selftext' in tag_list[check]['conditions'][condition]):
+                if ('selftext' in tags[check]['conditions']
+                        [condition]):
                     if not (re.match(
-                        tag_list[check]['conditions'][condition]['selftext'],
-                            post.selftext, re.IGNORECASE)):
+                        tags[check]['conditions'][condition]
+                            ['selftext'], post.selftext, re.IGNORECASE)):
                         continue
 
                 # No continues were hit, it's a match.
                 print (datetime.now().strftime("%H:%M:%S %m/%d/%y") +
-                       ": tagging " + post.id + " with " +
-                       tag_list[check]['css_class'] + ".")
+                        ": tagging " + post.id + " with " +
+                        tags[check]['css_class'] + ".")
 
                 post.set_flair(flair_css_class=
-                               tag_list[check]['css_class'])
+                                tags[check]['css_class'])
 
                 collection.insert(
                     {'post_id': post.id,
-                     'tagged_as': tag_list[check]['css_class'],
-                     'processed_on': datetime.now()})
+                        'tagged_as': tags[check]['css_class'],
+                        'processed_on': datetime.now()})
 
                 matched = True
                 break
         if not matched:
             print (datetime.now().strftime("%H:%M:%S %m/%d/%y") +
-                   ": no match for " + post.id + ".")
+                    ": no match for " + post.id + ".")
             collection.insert({'post_id': post.id,
-                               'match_type': 'none',
-                               'processed_on': datetime.now()})
+                                'match_type': 'none',
+                                'processed_on': datetime.now()})
 
-os.remove(lockpath)
+
+def load_config(section='main'):
+    """Load the specified config section, or main by default"""
+    try:
+        path = str(sys.path[0])
+        ret_dict = yaml.load(file(path+'/tagbot.yaml', 'r'))[section]
+    except AttributeError:
+        print "Config file not found, not readable, or incomplete."
+        sys.exit()
+
+    return ret_dict
+
+
+def write_lockfile(lockpath):
+    """Write a lockfile with information about current bot instance"""
+    if os.path.exists(lockpath):
+        print "Already running"
+        return False
+
+    lockfile = open(lockpath, 'w')
+    lockfile.write('Bot instance started at: ' +
+                   datetime.now().strftime("%H:%M:%S %m/%d/%y") + "\n")
+    lockfile.close()
+    return True
+
+
+if __name__ == "__main__":
+    main()
